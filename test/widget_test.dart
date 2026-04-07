e37e2fd3b1be10e965ee698b400aacfd8fa62beb
SASANK <sasank4pr@gmail.com>
2026-04-07 12:15:17 +0530

    working with windows too

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutternew/main.dart';
-import 'package:flutternew/login_page.dart';
 
 void main() {
   testWidgets('Login page smoke test', (WidgetTester tester) async {
